b681f4771f9cbca9244bd2893bf8728cf34a2ef8
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2024-08-14 10:45:43 +0200

    add task-02

--- src/scripts/task-02.ts
+++ src/scripts/task-02.ts
@@ -1,1 +1,43 @@
+console.log('task-02');
+console.log('');
+
+interface IInventory {
+  items: string[];
+  add(itemName: string): void;
+  remove(itemName: string): void;
+}
+
+type InventoryAction = (itemName: string) => void;
+
+const inventory: IInventory = {
+  items: ['Knife', 'Gas mask'],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
+
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+
+    this.items = this.items.filter(item => item !== itemName);
+  },
+};
+
+const invokeInventoryAction = function (itemName: string, action: InventoryAction) {
+  console.log(`Invoking action on ${itemName}`);
+  action(itemName);
+};
+
+invokeInventoryAction('Medkit', inventory.add.bind(inventory));
+// Invoking action on Medkit
+// Adding Medkit to inventory
+
+console.log(inventory.items); // ['Knife', 'Gas mask', 'Medkit']
+
+invokeInventoryAction('Gas mask', inventory.remove.bind(inventory));
+// Invoking action on Gas mask
+// Removing Gas mask from inventory
+
+console.log(inventory.items); // ['Knife', 'Medkit']
+
 export {};
